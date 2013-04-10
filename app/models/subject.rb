2e4278961fa867e96d90a97fdce974c05b32c403
Kiza <kiza@Kizas-MacBook-Pro.local>
2013-04-10 09:22:09 +0800

    show

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -7,4 +7,5 @@ class Subject < ActiveRecord::Base
   scope :visible, where(:visible => true)
   scope :invisible, where(:visible => false)
   scope :search, lambda{|query| where(["name LIKE ?", "%#{query}"])}
+  
 end
