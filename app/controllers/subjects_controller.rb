0ad2b389bd8744c640296f19dc2d636e71fc7d74
Kiza <chuxiwen1987@hotmail.com>
2013-04-17 18:13:56 +0800

    update

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,5 +1,15 @@
 class SubjectsController < ApplicationController
 
+  # handle url access without action
+  def index
+  	# call list action
+  	list
+
+  	# default it is going to try to render 'index' template
+  	# overwite with this
+  	render('list')
+  end
+
   def list
     @subjects = Subject.order("subjects.position ASC")
   end
